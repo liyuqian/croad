410a1c374347231c2ceebb7dfe0d74b9d1fc0b58
Yuqian Li <liyuqian79@gmail.com>
2024-03-09 23:59:20 -0800

    Add an empty roadart
    All the code is auto-generated.

---
+++ roadart/bin/roadart.dart
@@ -0,0 +1,5 @@
+import 'package:roadart/roadart.dart' as roadart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${roadart.calculate()}!');
+}
---
+++ roadart/test/roadart_test.dart
@@ -0,0 +1,8 @@
+import 'package:roadart/roadart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
