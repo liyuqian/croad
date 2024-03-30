32b7bd18dcc7a0980a0ab34f86be39b0e17f730a
Yuqian Li <liyuqian79@gmail.com>
2024-03-30 11:56:08 -0700

    Check tfrecord with lines

--- roadpy/check_tfrecord.py
+++ roadpy/check_tfrecord.py
@@ -1,8 +1,10 @@
+import math
 import tensorflow as tf
 import cv2
 import sys
 
-from tfrecord_setting import TFRECORD_PATH
+from tfrecord_setting import TFRECORD_PATH, TARGET_W, TARGET_H
+
 
 def check_tfrecord():
     """
@@ -29,7 +31,44 @@ def check_tfrecord():
         label = example.features.feature["label"].float_list.value
         print(f"label={label}")
         image = tf.image.decode_png(image, channels=3)
-        cv2.imshow("image", image.numpy())
+        image_np = image.numpy()
+        point = (int(label[0] * TARGET_W), int(label[1] * TARGET_H))
+        cv2.drawMarker(
+            image_np,
+            point,
+            (0, 0, 255),  # bgr
+            markerType=cv2.MARKER_DIAMOND,
+            markerSize=20,
+            thickness=2,
+        )
+
+        LENGTH = 200
+        left = label[2] * math.pi / 2
+        cv2.line(
+            image_np,
+            point,
+            (
+                point[0] - int(LENGTH * math.sin(left)),
+                point[1] + int(LENGTH * math.cos(left)),
+            ),
+            (255, 0, 0),  # bgr
+            thickness=2,
+        )
+
+        right = label[3] * math.pi / 2
+        cv2.line(
+            image_np,
+            point,
+            (
+                point[0] + int(LENGTH * math.sin(right)),
+                point[1] + int(LENGTH * math.cos(right)),
+            ),
+            (255, 0, 0),  # bgr
+            thickness=2,
+        )
+
+        cv2.imshow("image", image_np)
         cv2.waitKey(0)
+
 
 check_tfrecord()
